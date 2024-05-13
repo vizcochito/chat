ee1eddb6443c5e838131ec55ed99bb14400aa841
vizcochito <v.barria.j@gmail.com>
2024-05-13 21:08:08 +0200

    mensaje

--- chat.py
+++ chat.py
@@ -1,2 +1,3 @@
 print("Victor: Hola Pablo ¿Por qué miras la ventana?")
 print(" Pablo: Wena Victor! Había un wn bañandos en la calle.")
+print("Victor: ohhh vivirán en esa casita?, cuánta gente ha salido?, ves no se necesita mucho para ser feliz")
