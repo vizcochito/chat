c1686741830e4e3070a35a3fe60263e73410c765
oOAXEZzOo <pguimera11@gmail.com>
2024-05-13 21:10:01 +0200

    respuesta2

--- chat.py
+++ chat.py
@@ -1,3 +1,4 @@
 print("Victor: Hola Pablo ¿Por qué miras la ventana?")
 print(" Pablo: Wena Victor! Había un wn bañandos en la calle.")
 print("Victor: ohhh vivirán en esa casita?, cuánta gente ha salido?, ves no se necesita mucho para ser feliz")
+print("Pablo: Brigido wn son como los 7 enanitos en la misma casa. la deben transportar en ese carrito azul. Es como el bolsillo de Doraemon")
